1d3d30d29d0b37382d52bce8d7577387b514ac9b
Joey Rosztoczy <josephrosztoczy@Josephs-MacBook-Pro-3.local>
2018-03-03 20:50:06 -0700

    clean up error handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     // First argument is always the program binary, we want two additional arguments
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Couldn't parse arguments: {}", err);
+        process::exit(1);
+    });
 
     let mut f = File::open(config.filename).expect("File not found");
     let mut contents = String::new();
@@ -21,15 +25,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         match args.len() == 3 {
             true => {
                 let query = args[1].clone();
                 let filename = args[2].clone();
                 println!("Query Target: {:?}, Search For: {:?}", query, filename);
-                return Config { query, filename };
+                return Ok(Config { query, filename });
             }
-            _ => panic!("Please provide exactly one file to query and one term to search for"),
+            _ => return Err("Please provide exactly one file to query and one term to search for"),
         };
     }
 }
